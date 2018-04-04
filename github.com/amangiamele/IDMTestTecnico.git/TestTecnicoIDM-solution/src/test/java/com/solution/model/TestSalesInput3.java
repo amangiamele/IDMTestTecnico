be9c7ca637be9e530470fa5cc63ea8136465dcd5
antonio.mangiamele <antonio.mangiamele@WIN-THFL6D6M0R.corp.avanade.org>
2018-04-04 23:03:07 +0200

    Versione 4 - Rivedere l'arrotondamento (non fa l'arrotondamento per eccesso a 0,05)

--- github.com/amangiamele/IDMTestTecnico.git/TestTecnicoIDM-solution/src/test/java/com/solution/model/TestSalesInput3.java
+++ github.com/amangiamele/IDMTestTecnico.git/TestTecnicoIDM-solution/src/test/java/com/solution/model/TestSalesInput3.java
@@ -8,14 +8,19 @@ import org.junit.Test;
 public class TestSalesInput3 {
 	private Sales sales = new Sales();
 	
+	private Product importedProfume;
+	private Product bottleProfumes;
+	private Product headPills;
+	private Product importedBoxChocolate;
+	
 	@Before
 	public void init() {
 		sales = new Sales();
 		
-		Product importedProfume = new Product(0.15, "Imported Profume", 27.99);
-		Product bottleProfumes = new Product(0.1, "Bottle Profume", 18.99);
-		Product headPills = new Product(0.0, "packet of headache pills", 9.75);
-		Product importedBoxChocolate = new Product(0.05, " imported box of chocolates", 11.25);
+		importedProfume = new Product(0.15, "Imported Profume", 27.99);
+		bottleProfumes = new Product(0.1, "Bottle Profume", 18.99);
+		headPills = new Product(0.0, "packet of headache pills", 9.75);
+		importedBoxChocolate = new Product(0.05, " imported box of chocolates", 11.25);
 		
 		sales.buyProduct(importedProfume);
 		sales.buyProduct(bottleProfumes);
@@ -31,5 +36,45 @@ public class TestSalesInput3 {
 	@Test
     public void priceInput3() {
 		assertEquals(74.68 , sales.price(), 0);
+    }
+	
+	@Test
+    public void taxesImportedProfumeInput3() {
+		assertEquals(4.2 , importedProfume.getAmountTax(), 0);
+    }
+	
+	@Test
+    public void taxesProfumeInput3() {
+		assertEquals(1.9 , bottleProfumes.getAmountTax(), 0);
+    }
+	
+	@Test
+    public void taxesHeadPillsInput3() {
+		assertEquals(0.0 , headPills.getAmountTax(), 0);
+    }
+	
+	@Test
+    public void taxesImportedBoxChocolatenput3() {
+		assertEquals(0.6 , importedBoxChocolate.getAmountTax(), 0);
+    }
+	
+	@Test
+    public void priceImportedProfumeInput3() {
+		assertEquals(32.19 , importedProfume.getPrice(), 0);
+    }
+	
+	@Test
+    public void priceProfumeInput3() {
+		assertEquals(20.89 , bottleProfumes.getPrice(), 0);
+    }
+	
+	@Test
+    public void priceHeadPillsInput3() {
+		assertEquals(9.75 , headPills.getPrice(), 0);
+    }
+	
+	@Test
+    public void priceImportedBoxChocolatenput3() {
+		assertEquals(11.85 , importedBoxChocolate.getPrice(), 0);
     }
 }
