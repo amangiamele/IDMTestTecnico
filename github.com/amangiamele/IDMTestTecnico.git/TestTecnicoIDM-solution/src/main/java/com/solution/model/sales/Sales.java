71603ebce7632d3765c33c0e35ffcbbb82993fae
antonio.mangiamele <antonio.mangiamele@WIN-THFL6D6M0R.corp.avanade.org>
2018-04-05 16:30:51 +0200

    Revisione getPrice e getTaxes oggetto Sales considerando il numero di ordini

--- github.com/amangiamele/IDMTestTecnico.git/TestTecnicoIDM-solution/src/main/java/com/solution/model/sales/Sales.java
+++ github.com/amangiamele/IDMTestTecnico.git/TestTecnicoIDM-solution/src/main/java/com/solution/model/sales/Sales.java
@@ -26,22 +26,21 @@ public class Sales {
 	}
 
 	public void buyProduct(Product product) {
+		if(product == null) {
+			return;
+		}
+		Integer numberProduct;
+		String productName = product.getName();
+		
+		if(this.productsNumber.containsKey(product.getName())) {
+			numberProduct = this.productsNumber.get(productName);
+			numberProduct = numberProduct + 1;
+			this.productsNumber.put(productName, numberProduct);
+			return;
+		}
+		
 		this.products.add(product);
-//		if(product == null) {
-//			return;
-//		}
-//		Integer numberProduct;
-//		String productName = product.getName();
-//		
-//		if(this.productsNumber.containsKey(product.getName())) {
-//			numberProduct = this.productsNumber.get(productName);
-//			numberProduct = numberProduct + 1;
-//			this.productsNumber.put(productName, numberProduct);
-//			return;
-//		}
-//		
-//		this.products.add(product);
-//		this.productsNumber.put(productName, new Integer(1));
+		this.productsNumber.put(productName, new Integer(1));
 		
 	}
 	
@@ -59,20 +58,31 @@ public class Sales {
 	
 	public double getTaxes() {
 		double totalTax = 0.0;
+		double totalPruductTaxes = 0.0;
+		int productNumber = 0;
 		Product tmp;
+		
 		for(int i = 0; i < products.size(); i++) {
 			tmp = products.get(i);
-			totalTax = totalTax + tmp.getAmountTax();
+			productNumber = this.getNumberProduct(i);
+			totalPruductTaxes = tmp.getAmountTax() * productNumber;
+			totalTax = totalTax + totalPruductTaxes;
 		}
 		return Math.round(totalTax * 100.0) / 100.0;
 	}
 	
 	public double price() {
 		double totalPrice = 0.0;
+		double totalPruductPrice = 0.0;
+		int productNumber = 0;
 		Product tmp;
+		
 		for(int i = 0; i < products.size(); i++) {
 			tmp = products.get(i);
-			totalPrice = totalPrice + tmp.getPrice();
+			productNumber = this.getNumberProduct(i);
+			totalPruductPrice = tmp.getPrice() * productNumber;
+			totalPrice = totalPrice + totalPruductPrice;
+			
 		}
 		return Math.round(totalPrice * 100.0) / 100.0;
 	}
